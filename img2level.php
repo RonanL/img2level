ee9236b38b794fb957c2ca828b9f5aac985ce081
rleroy <rleroy@clever-age.com>
2012-12-04 15:11:50 +0100

    command line options

--- img2level.php
+++ img2level.php
@@ -1,6 +1,60 @@
+#!/usr/bin/php
 <?php
 
-$file = "img.png";
+$options['file'] = "img.png";
+
+$commands = array(
+    'f' => 'file',
+    'd' => 'dir',
+    'h' => 'help',
+    't' => 'target',
+    'c' => 'config',
+);
+
+foreach ($argv as $key => $arg){
+    if (substr($arg, 0, 2) == '--'){
+        if (strpos($arg, '=')){
+            $options[substr($arg, 2, strpos($arg, '=') - 2)]=substr($arg, strpos($arg, '=') + 1);
+        }
+        else{
+            $options[substr($arg, 2)]=true;
+        }
+    }
+    else if (substr($arg, 0, 1) == '-'){
+        for ($i = 1; $i < strlen($arg); $i++){
+            if (!isset($argv[$key + 1]) || substr($argv[$key + 1], 0, 1) == '-'){
+                $options[$commands[substr($arg, $i, 1)]] = true;
+            }
+            else{
+                $options[$commands[substr($arg, $i, 1)]] = $argv[$key + 1];
+            }
+        }
+    }
+}
+
+if (@$options['help']){
+    print "Available options:
+-f <file>, --file=<file> 
+    Specifies the source file to convert
+
+-d <directory>, --dir=<directory> //TODO
+    Converts all the files in the specified directory
+
+-h, --help
+    Displays this message
+
+-t <file,directory>, --target=<file,directory> //TODO
+    Write the result in the target file or directory.
+    If the target is a directory, a file will be created for each source image.
+
+-c <file>, --config=<file> //TODO
+    Specifies the config file to use
+
+";
+    exit;
+}
+
+$file = $options['file'];
 
 $size = getimagesize($file);
 $width = $size[0];
@@ -24,10 +78,9 @@ $level = array();
 for ($i=0; $i <$width; $i++){ 
     for ($j=0; $j<$height; $j++){
         $rgb = imagecolorat($im, $i, $j);
-	$level[$j][$i] = (int)!(bool) $rgb;
+        $level[$j][$i] = (int)!(bool) $rgb;
         //$color = imagecolorsforindex($im, $rgb);
     }
 }
 
-print "\n".json_encode($level)."\n";
-?>
+print "\n".str_replace('],', "],\n", json_encode($level))."\n";

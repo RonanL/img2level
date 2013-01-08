5ca22c62903dbdd6fa3f8fcdbf65f72d42f5e9da
rleroy <ronan.leroy@gmail.com>
2013-01-08 12:43:00 +0100

    Small update of example config file

--- config.php
+++ config.php
@@ -3,6 +3,7 @@ $config = array(
   'colors' => array(
     '000000' => array('value' => 1),
     'ffffff' => array('value' => 0),
-    'ff0000' => array('value' => 2, 'object' => array('type' => 'monster')),
+    'ff0000' => array('value' => 0, 'object' => array('type' => 'monster')),
+    '00ff00' => array('value' => 0, 'object' => array('type' => 'player_start')),
   ),
 );
